4c0c113853a23329398780f9c2340aafd13df198
ylro <ylro@mcnc>
2024-05-21 17:56:16 +0900

    init commit

---
+++ jpa/src/main/java/jpa/web/TestController.java
@@ -0,0 +1,14 @@
+package jpa.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/index")
+	public String test() {
+		return "index";
+	}
+
+}
